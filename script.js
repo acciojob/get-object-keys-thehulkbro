bd7b5bf95534ef53308abeb178ff248e19925c61
Abhijit Das <46588834+thehulkbro@users.noreply.github.com>
2024-08-29 22:02:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-let student ={
-	name : "abhi"
+// Create an object called student with a property called name
+const student = {
+  name: "John Doe"
 };
-	Object.prptotype.getkeys= function() {
-	return Object.keys(this);
-	};
-//how to get value of keys
-let keys = student.getkeys();
-console.log(keys);
+
+// Add a method to the Object prototype called getKeys
+Object.prototype.getKeys = function() {
+  return Object.keys(this);
+};
+
+// Use the getKeys method on the student object
+const keys = student.getKeys();
+console.log(keys); // Output: ["name"]
